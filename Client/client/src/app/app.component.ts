ce87223dd0c5bb4d836eb97a5f5eadaf88c2b57c
Alfred Rwagaju <rwagajua18@icloud.com>
2022-05-24 23:37:03 -0400

    http calls

--- Client/client/src/app/app.component.ts
+++ Client/client/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  users: any;
+  //baseUrl = 'https://localhost:5001/';
+
+
+  constructor(private httpService: HttpClient ){}
+
+  ngOnInit(){
+
+    this.getUsers();
+    
+  }
+
+
+ 
+
+  getUsers(){
+    this.httpService.get('https://localhost:5001/user').subscribe(
+      response => {
+        this.users = response;
+      }, error => {
+        console.log(error)
+      })
+
+  }
 }
